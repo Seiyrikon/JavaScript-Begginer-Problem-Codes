d93d7c1b7d731c6a11cae4161b8566e5cfba18ff
Seiyrikon <rickygalpo7777@gmail.com>
2023-04-17 21:42:33 +0800

    added a loop function on 4

--- 4/script.js
+++ 4/script.js
@@ -1,21 +1,21 @@
 const start = document.getElementById("start"),
-      rotate = document.getElementById("rotate"),
-      other = document.getElementById("other"),
-      matched = document.getElementById("matched");
+    rotate = document.getElementById("rotate"),
+    other = document.getElementById("other"),
+    loop = document.getElementById("loop"),
+    matched = document.getElementById("matched");
 
 let arrayStorage = [];
 let wordStorage = '';
 
-
-
 function startRotate(word) {
     word = window.prompt("Enter a word to rotate");
-    convertToArray = Object.assign([],word);
+    convertToArray = Object.assign([], word);
     arrayStorage = convertToArray;
     wordStorage = word;
     document.getElementById("word").innerText = convertToArray.join('');
     start.style.display = 'none';
     rotate.style.display = 'inline';
+    loop.style.display = 'inline';
 }
 
 function rotateWord() {
@@ -27,6 +27,7 @@ function rotateWord() {
         matched.style.display = 'inline';
         rotate.style.display = 'none';
         other.style.display = 'inline';
+        loop.style.display = 'none';
     }
 }
 
@@ -34,4 +35,25 @@ function tryOtherWord() {
     matched.style.display = 'none';
     other.style.display = 'none';
     startRotate();
+}
+
+const sleep = (time) => {
+    return new Promise((resolve) => setTimeout(resolve, time))
+  }
+
+const loopWord = async () => {
+    for (x = 0; x < arrayStorage.length; x++) {
+        lastCharacter = arrayStorage.pop()
+        arrayStorage.unshift(lastCharacter);
+        await sleep(1000);
+        displayedWord = document.getElementById("word").innerText = arrayStorage.join('');
+
+        console.log(arrayStorage);
+        if (wordStorage === displayedWord) {
+            matched.style.display = 'inline';
+            rotate.style.display = 'none';
+            other.style.display = 'inline';
+            loop.style.display = 'none';
+        }
+    }
 }
