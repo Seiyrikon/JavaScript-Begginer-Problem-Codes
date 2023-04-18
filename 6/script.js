117d2056593fdf56a7d60f318a84c1fecbe303a8
Seiyrikon <rickygalpo7777@gmail.com>
2023-04-19 02:16:01 +0800

    minor fix on 6

--- 6/script.js
+++ 6/script.js
@@ -1,8 +1,11 @@
-const monthArr = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const monthArr = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+      check = document.getElementById("check"),
+      list = document.getElementById("list"),
+      reloadPage = document.getElementById("reload"),
+      year = document.getElementById("year");
 
 function yearLoop() {
     const yearInput = document.getElementById("year").value;
-
     if (!isNaN(yearInput)) {
         if (yearInput.length !== 4) {
             document.getElementById("msgContainer").innerText = "Enter a 4-digit year";
@@ -11,20 +14,27 @@ function yearLoop() {
             for (x = date.getFullYear(); x <= 2050; x++) {
                 date.setFullYear(x);
                 if (date.getDay() !== 0) {
-                    console.log(`${monthArr[date.getMonth()]} ${date.getDate()}st of ${x} is not a sunday`)
                 } else {
-                    console.log(`${monthArr[date.getMonth()]} ${date.getDate()}st of ${x} is a sunday`)
+                    const createElement = document.createElement("h2");
+                    let content = document.createTextNode(`${monthArr[date.getMonth()]} ${date.getDate()}st of ${x} is a sunday`);
+                    createElement.appendChild(content);
+                    const msgContainer = document.getElementById("msgContainer");
+                    document.body.insertBefore(createElement, msgContainer);
+                    document.getElementById("title").innerText = `List of years from ${yearInput} to 2050 with January 1st as sunday.`
                 }
             }
         }
     } else {
         document.getElementById("msgContainer").innerText = "Please get help!";
     }
+    check.style.display = "none";
+    list.style.display = "none";
+    reloadPage.style.display = "inline";
+    year.style.display = "none";
 }
 
 function dayChecker() {
     const yearInput = document.getElementById("year").value;
-
     if (!isNaN(yearInput)) {
         if (yearInput.length !== 4) {
             document.getElementById("msgContainer").innerText = "Enter a 4-digit year";
@@ -39,5 +49,12 @@ function dayChecker() {
     } else {
         document.getElementById("msgContainer").innerText = "Please get help!";
     }
+    check.style.display = "none";
+    list.style.display = "none";
+    reloadPage.style.display = "inline";
+    year.style.display = "none";
 }
 
+function reload() {
+    location.reload();
+}
